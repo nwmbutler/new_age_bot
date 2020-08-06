83cdb788026034639ee0cc6a07638332b7965301
Nick Butler <nwmbutler@gmail.com>
2020-08-06 14:40:54 +0100

    succesfully getting tweets using params

--- bot.js
+++ bot.js
@@ -3,3 +3,16 @@ console.log('the bot is starting')
 var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
+
+var params = {
+   q: 'Makers academy', 
+   count: 5
+};
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; i++)
+    console.log(tweets[i].text)
+  };
