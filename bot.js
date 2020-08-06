e2d7e8b0c52e09f379401c04426fc91726d681ed
Nick Butler <nwmbutler@gmail.com>
2020-08-06 15:50:25 +0100

    bot2 deleted

--- bot.js
+++ bot.js
@@ -4,15 +4,24 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
-var params = {
-   q: 'Makers academy', 
-   count: 5
-};
+tweetIt();
+setInterval(tweetIt, 1000*60*60)
 
-T.get('search/tweets', params, gotData);
+function tweetIt() {
 
-function gotData(err, data, response) {
-    var tweets = data.statuses;
-    for (var i = 0; i < tweets.length; i++)
-    console.log(tweets[i].text)
-  };
+  var post = "something";
+
+  var tweet = { 
+      status: post + " #mystical"
+  }
+
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("it worked!")
+    }
+  }
+}
