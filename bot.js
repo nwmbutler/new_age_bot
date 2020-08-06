7aa9e111e233e8372913a3395627010fd9f02e2f
Nick Butler <nwmbutler@gmail.com>
2020-08-06 14:24:18 +0100

    API keys added and hidden

--- bot.js
+++ bot.js
@@ -1,12 +1,5 @@
 console.log('the bot is starting')
 
 var Twit = require('twit');
-
-var T = new Twit({
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
-  })
+var config = require('./config');
+var T = new Twit(config);
